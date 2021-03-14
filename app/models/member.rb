02b9dd6975397c465ac95a4d2f88b5bc9ef8e8b9
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-03-14 17:22:08 -0400

    Done.

---
+++ app/models/member.rb
@@ -0,0 +1,19 @@
+class Member
+
+    attr_accessor :name, :power, :bio
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(params)
+        @name = params[:name]
+        @power = params[:power]
+        @bio = params[:bio]
+        self.class.all << self
+    end
+
+
+end
